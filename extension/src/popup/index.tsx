1107bf5aabf478ee4d89d26068b28df02c992db1
Saikiran <ksai41530@gmail.com>
2025-07-21 05:44:46 -0500

    Fix concurrent access issue

--- extension/src/popup/index.tsx
+++ extension/src/popup/index.tsx
@@ -136,3 +136,5 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
 // Updated: 2025-07-19 - Update dependencies
 
 // Updated: 2025-07-21 - Refactor database connection pooling
+
+// Updated: 2025-07-22 - Fix concurrent access issue
